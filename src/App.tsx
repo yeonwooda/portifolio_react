3b2e2525c245fa0e622c7a56f1f36459a2282891
yeonwooda <166900711+yeonwooda@users.noreply.github.com>
2024-07-13 15:49:32 +0900

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/organism/Layout";
+import Mainpage from "./components/organism/Mainpage";
+import AttendanceCheck from "./components/organism/AttendanceCheck";
+import Students from "./components/organism/Students";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/mainpage" Component={Mainpage} />
+          <Route path="/attendance_check" Component={AttendanceCheck} />
+          <Route path="/students" Component={Students} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
